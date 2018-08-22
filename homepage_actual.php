dca396f0b8ad89384e48f93589f338c8cd1b8cd0
tvolpert <timothyvolpert@gmail.com>
2018-08-22 10:11:22 -0500

    Initial commit

---
+++ homepage_actual.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN" lang="EN" dir="ltr">
+<head>
+<title>KGI Home</title>
+
+<!--/***********************************************
+* Simple Tree Menu- © Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/-->
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="imagetoolbar" content="no" />
+
+
+
+<link rel="stylesheet" href="styles/layout.css" type="text/css" />
+<!-- 3 Column Stylesheet Added To The Page And Not To The Layout.css -->
+<link rel="stylesheet" href="styles/3_column.css" type="text/css" />
+
+
+	
+<!-- Homepage Specific Elements -->
+<script type="text/javascript" src="scripts/jquery-1.4.1.min.js"></script>
+<script type="text/javascript" src="scripts/jquery-ui-1.7.2.custom.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.tabs.setup.js"></script>
+<link rel="stylesheet" href="styles/featured_slide.css" type="text/css"/>
+<!-- End Homepage Specific Elements -->
+
+<!-- Header CSS-->
+	<link rel="stylesheet" type="text/css" href="styles/CanHeader.css"/>
+<!-- End Header CSS-->
+
+	
+<body id="top">
+
+<table width="100%" border="0" cellpadding="" cellspacing="0" bgcolor="#192857">
+  <tr>
+    <td width="225"><img src="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/AgencyCommon/img/SLK_White_Gold.png" width="193" height="112" border="0"></td>
+    <td>
+		<p><span class="fontsize">Kansas Government Information (KGI) Online Library<br></span>
+			<span class="subtitle">An online collection of state government publications</span></p>
+		<p class="right">785-296-3296</p>
+	</td>
+  </tr>
+</table>
+
+<table width="100%" border="0" cellspacing="0" cellpadding="10">
+  <tr>
+    <td bgcolor="#238fcf"; >&nbsp;
+		<a href="http://cdm16884.contentdm.oclc.org/cdm/" class="NavBar"><span>KGI Home</span></a> 
+		<a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/agencylist/AgencyList.php" class="NavBar">&nbsp;|&nbsp;&nbsp;<span>Select an Agency</span></a> 
+		<a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/History_Pages/Index_History.php" class="NavBar">&nbsp;|&nbsp;&nbsp;<span>Historical State Agencies</span></a> 
+		<a href="http://kslib.info/" class="NavBar">&nbsp;|&nbsp;&nbsp;<span>State Library Homepage</span></a> 
+		<a href="http://catalog.library.ks.gov" class="NavBar" target="_blank"  >&nbsp;|&nbsp;&nbsp;<span>State Library Catalog</span></a> 
+		<a href="http://kslib.info/ask" class="NavBar">&nbsp;|&nbsp;&nbsp;<span>Ask a Librarian</span></a>
+	</td>
+  </tr>
+</table>
+  </div>
+<!-- ####################################################################################################### -->
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="featured_slide" class="clear">
+    <!-- ###### -->
+    <div class="overlay_left"></div>
+    <div id="featured_content">
+      <div class="featured_box" id="fc1"><img src="images/lib/stacks.jpg" alt="" />
+        <div class="floater">
+          <h2>What is KGI? </h2>
+          <p>An Online Library of born-digital and digitally rendered material published by Kansas government agencies. </p>
+          <p class="readmore"><a href="#"></a></p>
+        </div>
+      </div>
+      <div class="featured_box" id="fc2"><a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/treesearch/index.php?mc=mobile"><img src="images/lib/periodicals-index.png" alt="" /></a>
+        <div class="floater">
+          <h2>Browse Agency Collections</h2>
+          <p>Don't know where to start?  Casually peruse the holdings of each agency in the library</p>
+          <p class="readmore"><a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/treesearch/index.php?mc=mobile">Take Me There &raquo;</a></p>
+        </div>
+      </div>
+      <div class="featured_box" id="fc3"><a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/History_Pages/Index_History.php"><img src="images/lib/lawbooks.png" alt="" /></a>
+        <div class="floater">
+          <h2>Historical Collections c. 1856-1980</h2>
+          <p>Interested in the old stuff? Check out the government documents from long ago and from agencies that have been disbanded.</p>
+          <p class="readmore"><a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/History_Pages/Index_History.php">Take Me There &raquo;</a></p>
+        </div>
+      </div>
+      <div class="featured_box" id="fc4"><a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/RSS_Blog/Blog_RSS.php"><img src="images/lib/Senate.jpg" alt="" /></a>
+        <div class="floater">
+          <h2>New and Noteworthy!</h2>
+          <p>Keep up with the latest updates to the library and check out our blog, which highlights noteworthy collections</p>
+          <p class="readmore"><a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/RSS_Blog/Blog_RSS.php">Take Me There &raquo;</a></p>
+        </div>
+      </div>
+      <div class="featured_box" id="fc5"><a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/Agency_History/AgentHist.php"><img src="images/lib/FamilyPhoto.jpg" alt="" /></a>
+        <div class="floater">
+          <h2>History of Kansas Government Agencies</h2>
+          <p>Click here for resources relating to the history of Kansas government agencies</p>
+          <p class="readmore"><a href="http://cdm16884.contentdm.oclc.org/ui/custom/default/collection/default/resources/custompages/Agency_History/AgentHist.php">Take Me There &raquo;</a></p>
+        </div>
+      </div>
+    </div>
+	
+	<!--Feature Bar, bottom boxes text-->
+		<ul id="featured_tabs">
+		  <li><a href="#fc1">About KGI</a></li>
+		  <li><a href="#fc2">Browse Agency Collections</a></li>
+		  <li><a href="#fc3">Historical Collections</a></li>
+		  <li><a href="#fc4">New and Noteworthy</a></li>
+		  <li class="last"><a href="#fc5">History of Kansas Government Agencies</a></li>
+		</ul>
+	
+	<!--Original, Internal Texbox references---
+		<ul id="featured_tabs">
+		  <li><a href="#fc1">About KGI</a></li>
+		  <li><a href="#fc2">Browse Agency Collections</a></li>
+		  <li><a href="#fc3">Historical Collections</a></li>
+		  <li><a href="#fc4">New and Noteworthy</a></li>
+		  <li class="last"><a href="#fc5">History of Kansas Government Agencies</a></li>
+		</ul>
+	--->
+    <div class="overlay_right"></div>
+    <!-- ###### -->
+  </div>
+</div>
+
+<!-- ####################################################################################################### -->
+<div class="wrapper row3">
+  <div class="rnd">
+    <div id="container" class="clear">
+      <!-- ####################################################################################################### -->
+      <!-- ############ -->
+      <div id="content">
+        <h1 class="title">Keyword Search
+		<br> 
+			<form method="GET" action="http://cdm16884.contentdm.oclc.org/cdm/search/">
+				<input type="text" name="searchterm" size="100" style="height:30px;font-size:15pt">
+				<input type="submit" value="Submit" class="button">
+			</form>
+		</h1>
+      </div>
+      <!-- ############ -->
+      <!-- ####################################################################################################### -->
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+
+<!-- ####################################################################################################### -->
+<div class="wrapper row3">
+  <div class="rnd">
+    <div id="container" class="clear">
+      <!-- ####################################################################################################### -->
+      <div id="left_column">
+        <div class="holder">
+          <h2 class="title"></h2>
+			<iframe name="awindow" style="border:0px ridge black" width=1 height=500 src="Bar Test.php" align="left"></iframe>
+			<br><br><br><br><br><br><br>
+		</div>		
+      </div>
+      <!-- ############ -->
+      <div id="content">
+        <h1 class="title"><center>Browse Agency Collections</center></h1>
+			<iframe name="cwindow" style="border:0px" width=875 height=1000 src="browse-page-stripped.php" align="right"></iframe>
+
+      </div>
+      <!-- ############ -->
+      <div id="right_column">
+        <div class="holder">
+          <h2 class="title"></h2>
+          <p></p>
+			<iframe name="cwindow" style="border:0px ridge black" width=225 height=300 src="https://us.libraryh3lp.com/chat/slkreference@chat.libraryh3lp.com?skin=18078&identity=Lib" align="center"></iframe>
+        </div>
+      </div>
+      <!-- ####################################################################################################### -->
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper row4">
+  <div class="rnd">
+    <div id="footer" class="clear">
+      <!-- ####################################################################################################### -->
+      <div class="fl_left clear">
+        <div class="fl_left center"><img src="images/worldmap.gif" alt="" /><br />
+          <a href="https://www.google.com/maps/place/State+Library+of+Kansas/@39.048139,-95.677996,17z/data=!3m1!4b1!4m2!3m1!1s0x87bf031ab898b8a1:0x7757859dd124e197">Find Us With Google Maps &raquo;</a></div>
+        <address>
+			State Library of Kansas<br />
+			300 SW 10th Ave # 312N<br />
+			Topeka, KS<br />
+			66612<br />
+			<br />
+			Tel: (785) 296-3296<br />
+			Email: <a href="mailto:KSDOCS@ks.gov">KSDOCS@ks.gov</a>
+        </address>
+      </div>
+      <div class="fl_right">
+        <div id="social" class="clear">
+          <p>Stay Up to Date With What's Happening</p>
+          <ul>
+			<li><a style="background-position:0 0;" href="https://twitter.com/libraryofks">Twitter</a></li>
+            <li><a style="background-position:-142px 0;" href="https://www.facebook.com/statelibraryofkansas">Facebook</a></li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- ####################################################################################################### -->
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -- Grant Footer-->
+
+<div class="wrapper row4">
+  <div class="rnd">
+    <div id="gfooter" class="clear">
+      <!-- ####################################################################################################### -->
+      <div class="fl_left clear">
+        <div class="fl_left center">
+			<a href="https://www.imls.gov/">
+				<img src="images/imls_logo_2c.jpg" alt="IMLS logo" />
+			</a> <br>
+          </div>
+      </div>
+      <div class="fl_right">
+        <div id="gtext" class="clear">
+		 <p>
+			This project was made possible in part by the Institute of Museum and Library Services. The views, findings, 
+			conclusions or recommendations expressed in this website do not necessarily represent those of the Institute 
+			of Museum and Library Services.
+		</p>
+       </div>
+
+      </div>
+      <!-- ####################################################################################################### -->
+	  
+    </div>
+  </div>
+</div>
+
+<!-- ####################################################################################################### -- KsHS Footer-->
+
+<div class="wrapper row4">
+  <div class="rnd">
+    <div id="gfooter" class="clear">
+      <!-- ####################################################################################################### -->
+      <div class="fl_left clear">
+        <div class="fl_left center">
+			<a href="https://kshs.org">
+				<img src="images/kshslogo.png" alt="Kansas State Historical Society" />
+			</a> <br>
+          </div>
+      </div>
+      <div class="fl_right">
+        <div id="gtext" class="clear">
+		 <p>
+			Our thanks to the Kansas Historical Society for lending us many of their historic government publications which are being digitized and added to the KGI Online Library collection.
+		</p>
+       </div>
+
+      </div>
+      <!-- ####################################################################################################### -->
+	  
+    </div>
+  </div>
+</div>
+</body>
+</body> 
+
+</html>
